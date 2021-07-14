23279e7afebbea44a02fd52855860a298765b8a2
Wagner Carestini <wcarestini@gmail.com>
2021-07-14 15:23:00 -0300

    Configuração inicial do projeto.

---
+++ src/test/java/com/mobil/MobilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
