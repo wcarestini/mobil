dfb28ef6ab8709b01f31a1e12d55a2be59b8c7e2
Wagner Carestini <wcarestini@gmail.com>
2021-07-15 15:27:58 -0300

    Criação das entidades.

---
+++ src/main/java/com/mobil/model/entity/Linha.java
@@ -0,0 +1,35 @@
+package com.mobil.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity(name = "Linhas")
+public class Linha {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id_linha")
+    private Long id;
+
+    private String codigo;
+
+    private String nome;
+
+    @OneToMany(mappedBy = "linha", fetch = FetchType.LAZY)
+    private List<Itinerario> itinerarios;
+}
---
+++ src/main/java/com/mobil/model/entity/Parada.java
@@ -0,0 +1,36 @@
+package com.mobil.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity(name = "Paradas")
+public class Parada {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id_parada")
+    private Long id;
+
+    @Column(columnDefinition = "decimal")
+    private Double latitute;
+
+    @Column(columnDefinition = "decimal")
+    private Double longitude;
+
+    @ManyToMany(mappedBy = "paradas")
+    private List<Itinerario> itinerarios;
+}
